155dca5d72b3d309894fc51e06108bbab9ac26bc
josephkipkemoi <jkemboe@gmail.com>
2022-07-14 11:35:08 -0400

    User can post to support team

--- src/components/Support.js
+++ src/components/Support.js
@@ -1,6 +1,8 @@
+import axios from '../lib/axios';
 import React, { useState } from "react";
 import styled from "styled-components";
 import { InputNumber } from "./Html";
+import { CheckSvgIcon, HelpSvgIcon, SendSvgIcon } from "./Svg";
 
 const StlyeSupport = styled.div`
     position: absolute;
@@ -30,12 +32,65 @@ const StyleSupportForm = styled.div`
         border-top-left-radius: 8px;
     }
 `
+
+const StyleConfirmation = styled.div`
+    position: absolute;
+    height: 402px;
+    background: #fff;
+    border-radius: 8px;
+    margin-top: 60px;
+    margin-left: 10px;
+    margin-right: 10px;
+`
 export default function Support() {
     const [display, setDisplay] = useState('none')
+    const [isMessageSent, setIsMessageSent] = useState(false)
+    const [isBtnDisabled, setIsBtnDisabled] = useState(false)
+  
+
     const SupportForm = () => {
         return (
             <StyleSupportForm className="bg-light" style={{ display: display }}>
-                <div className="d-flex justify-content-between align-items-center bg-success text-light p-3 round-header">
+                {isMessageSent ?  <MessageSentConfirmation/> : ''}               
+                <FormElements/>
+            </StyleSupportForm>
+        )
+    }
+
+    const confirmMessageSent = () => {
+        setIsMessageSent(false)
+        setDisplay('none')
+    }
+
+    const changeDisplay = () => {
+        setDisplay('block')
+        setIsBtnDisabled(false)
+    }
+
+    
+    const FormElements = () => {
+        const [formDetails, setFormDetails] = useState({
+            name: '',
+            email: '',
+            phone_number: '',
+            message: ''
+        });
+
+        const handleForm = (e) => setFormDetails((prev) => ({...prev, [e.target.name] : e.target.value}))
+
+        const submitMessage = async () => {
+       
+            const res = await axios.post('api/support', formDetails)
+    
+            if(res.status === 200) {
+                setIsMessageSent(true)
+                setIsBtnDisabled(true)
+            }    
+    
+        }
+        return (
+            <>
+             <div className="d-flex justify-content-between align-items-center bg-success text-light p-3 round-header">
                     <h6 >Leave us a message</h6>
                    <button 
                    style={{ border: 'none', background: 'none' }}
@@ -46,47 +101,89 @@ export default function Support() {
                 </div>
                 <div className="p-3 set-overflow">
                     <label htmlFor="name"><small>Your name (optional)</small></label>
-                    <input id="name" type="text" className="form-control mt-3 mb-3"/>
+                    <input 
+                    id="name" 
+                    type="text" 
+                    name="name"
+                    className="form-control mt-3 mb-3"
+                    onChange={handleForm}
+                    />
 
-                    <label htmlFor="email"><small>Email address</small></label>
-                    <input id="email" type="email" className="form-control mt-3 mb-3"/>
+                    <label htmlFor="email"><small>Email address *</small></label>
+                    <input 
+                    id="email" 
+                    type="email"
+                    name="email" 
+                    className="form-control mt-3 mb-3"
+                    onChange={handleForm}
+                    />
 
-                    <label htmlFor="number"><small>Phone Number</small></label>
+                    <label htmlFor="number"><small>Phone Number *</small></label>
                     <small className="d-block">Enter your phone number</small>
-                    <InputNumber className="form-control mt-3 mb-3"/>
+                    <InputNumber 
+                    id="number"
+                    name="phone_number"
+                    className="form-control mt-3 mb-3"
+                    onChange={handleForm}
+                    />
 
-                    <label htmlFor="message"><small>How can we help you?</small></label>
-                    <input type="text" className="form-control mt-3 mb-3" style={{ minHeight: 100 }}/>
+                    <label htmlFor="message"><small>How can we help you? *</small></label>
+                    <textarea 
+                    id="message"
+                    name="message"
+                    className="form-control mt-3 mb-3" 
+                    style={{ minHeight: 100 }}
+                    onChange={handleForm}
+                    />
 
                     <label htmlFor="attachments"><small>Attachments</small></label>
-                    <input type="file" className="form-control mt-3 mb-3"/>
+                    <small className="d-block">
+                        <i>Accepted formats: <b className="fw-bold">jpg, jpeg, png, pdf</b></i>
+                    </small>
+                    <input type="file" className="form-control mt-3 mb-3" disabled/>
                 </div>
+                <div className="p-3 bg-secondary">
+                    <button 
+                    className="btn btn-light w-100" 
+                    disabled={isBtnDisabled}
+                    onClick={submitMessage}
+                    >
+                        <SendSvgIcon width="16" height="16" style={{marginRight:4}}/>
+                        Send
+                    </button>
+                </div>
+            </>
+        )
+    }
 
-                <button className="btn btn-dark m-3 float-end">Send</button>
-            </StyleSupportForm>
+    const MessageSentConfirmation = () => {
+        return (
+            <StyleConfirmation className="p-4 text-center">
+                <h3 style={{ letterSpacing: 1 }}>
+                    Message Received
+                    <CheckSvgIcon width="62" height="62" style={{ display: 'block', margin: '32px auto', color: 'green' }}/>
+                </h3>
+                <span style={{ letterSpacing: 1 }}>One of your agents will contact you within 24 to 48 hours</span>
+                <button 
+                className="btn btn-success w-100 mt-4"
+                onClick={confirmMessageSent}
+                >
+                    Ok
+                </button>
+            </StyleConfirmation>
         )
     }
     return (
         <>
-          <StlyeSupport className="rounded-pill bg-warning" onClick={() => setDisplay('block')}>
+          <StlyeSupport className="rounded-pill bg-warning" onClick={changeDisplay}>
             <div className="d-flex justify-content-between align-items-center">
-                <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
-                fill="currentColor" 
-                className="bi bi-question-circle" 
-                viewBox="0 0 16 16"
-                style={{ marginLeft: 10 }}
-                >
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>
-                </svg>
+                <HelpSvgIcon width="24" height="24" style={{ marginLeft: 10 }}/>
                 <h5 style={{ marginTop: 3, marginRight: 10 }}>Help</h5>
             </div>
         </StlyeSupport> 
 
         <SupportForm/>
+
         </>
      
     )
