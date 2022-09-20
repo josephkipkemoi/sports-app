30e86131809c2b9b9b452cc089975b03b7f5f9ec
josephkipkemoi <jkemboe@gmail.com>
2022-09-20 19:48:27 +0300

    Register Service Worker

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,7 +19,6 @@ import Script from "next/script";
 import useAuth from "../hooks/auth";
 import Loader from "../components/Loader";
 import Image from "next/image";
-import sw from '../../service-worker'
 
 if(typeof document !== 'undefined') {
     if(!window.sayHello) {
@@ -87,8 +86,8 @@ export default function MyApp({ Component, pageProps }) {
             window.addEventListener('load', async () => {
               // try to register the service worker
               try {
-                  const reg = await navigator.serviceWorker.register(sw)
-                  console.log('Service worker registered')
+                  const reg = await navigator.serviceWorker.register('https://www.pinaclebet.com/public/service-worker.js')
+                  console.log('Service worker registered', reg)
               } catch (err) {
                 console.log('Service worker registration failed!', err)
               }
