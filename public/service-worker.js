771065966a583dfd9b1daf316caafcb59b63fa1a
josephkipkemoi <jkemboe@gmail.com>
2022-09-20 20:14:47 +0300

    Register Service Worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,26 @@
+// Pinaclebet ServiceWorker
+const cacheName = 'cache-v1'
+// List of files to be precached
+const precachedResources = ['/', '/history', '/profile', '/jackpot', '/soccer']
 
-    const cacheName = 'cache-v1'
-    // List of files to be precached
-    const precachedResources = ['/']
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (e) => {
+    console.log('Service worker install event!')
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precachedResources)))
+})
 
-            // When the service worker is installing, open the cache and add the precache resources to it
-            self.addEventListener('install', (e) => {
-                console.log('Service worker install event!')
-                e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precachedResources)))
-            })
-
-            // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-            self.addEventListener('fetch', (e) => {
-                console.log('Fetch intercepted for: ', e.request.url)
-                e.respondWith(
-                    caches.match(e.request).then((cachedResponse) => {
-                        if(cachedResponse) {
-                            return cachedResponse
-                        }
-
-                        return fetch(e.request)
-                    })
-                )
-            })
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (e) => {
+    console.log('Fetch intercepted!')
+    e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+        if(cachedResponse) {
+            return cachedResponse
+        }
+            return fetch(e.request)
+        })
+    )
+})
 
 
 
