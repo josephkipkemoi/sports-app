8bade4e7872bdf2fe552059e2d618452e08a5c14
josephkipkemoi <jkemboe@gmail.com>
2022-09-20 21:28:08 +0300

    Service Worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,73 +1,32 @@
 // Pinaclebet ServiceWorker
-import { warmStrategyCache } from 'workbox-recipes';
-import { CacheFirst } from 'workbox-strategies';
-import { registerRoute } from 'workbox-routing';
-import { CacheableResponsePlugin } from 'workbox-cacheable-response';
-import { ExpirationPlugin } from 'workbox-expiration';
 
-// Set up page cache
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
-
-warmStrategyCache({
-  urls: [
+const cacheName = 'cache-v1'
+// List of files to be precached
+const precachedResources = [ 
     '/', 
     '/history', 
     '/profile', 
     '/jackpot', 
     '/soccer'
-],
-  strategy: pageCache,
-});
+]
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (e) => {
+    console.log('Service worker install event!')
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precachedResources)))
+})
 
-// Set up asset cache
-registerRoute(
-    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-      cacheName: 'asset-cache',
-      plugins: [
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-      ],
-    }),
-  ); 
-
-  offlineFallback({
-    pageFallback: '/offline',
-  });
-// const cacheName = 'cache-v1'
-// // List of files to be precached
-// const precachedResources = [    
-//                             ]
-
-// // When the service worker is installing, open the cache and add the precache resources to it
-// self.addEventListener('install', (e) => {
-//     console.log('Service worker install event!')
-//     e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precachedResources)))
-// })
-
-// // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
-// self.addEventListener('fetch', (e) => {
-//     console.log('Fetch intercepted!')
-//     e.respondWith(
-//     caches.match(e.request).then((cachedResponse) => {
-//         if(cachedResponse) {
-//             return cachedResponse
-//         }
-//             return fetch(e.request)
-//         })
-//     )
-// })
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (e) => {
+    console.log('Fetch intercepted!')
+    e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+        if(cachedResponse) {
+            return cachedResponse
+        }
+            return fetch(e.request)
+        })
+    )
+})
 
 
 
