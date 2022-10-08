880f3d1cec1e4a50bb8fc5646090fcd5106f3ae9
josephkipkemoi <jkemboe@gmail.com>
2022-10-08 17:08:05 +0300

    Websockets Config

--- src/components/ChatBoxComponent.js
+++ src/components/ChatBoxComponent.js
@@ -231,6 +231,10 @@ const ChatBoxElement = ({ setCustomMsg }) => {
                 key: 'b36bb776d85f37fdff66',
                 cluster: 'ap2',
                 forceTLS: true,
+                encrypted: true,
+                enableTransports: ['ws', 'wss'],
+                disabledTransports: ['sockjs', 'xhr_polling', 'xhr_streaming'],
+                // wssPort: '' Https port
             })
 
             window.Echo.channel(`message-channel${userId}`)
---
+++ src/lib/echo.js
@@ -0,0 +1,17 @@
+import Echo from "laravel-echo";
+
+window.Pusher = require('pusher-js')
+
+const echo = window.Echo = new Echo({
+    broadcaster: 'pusher',
+    wsHost: window.location.hostname,
+    key: 'b36bb776d85f37fdff66',
+    cluster: 'ap2',
+    forceTLS: true,
+    encrypted: true,
+    enableTransports: ['ws', 'wss'],
+    disabledTransports: ['sockjs', 'xhr_polling', 'xhr_streaming'],
+    // wssPort: '' Https port
+})
+
+export default echo;
