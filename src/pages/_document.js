dd98ae799a66c67a5c9adfcc211d1b05699aba68
josephkipkemoi <jkemboe@gmail.com>
2023-03-02 18:19:10 +0300

    Serve CSS static files

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -26,8 +26,8 @@ export default class MyDocument extends Document {
                         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
                         crossOrigin="anonymous"
                     />              
-                    <link rel="stylesheet" href={`${config.FRONT_END_URL}css/bootstrap.css`}/>
-                    <link rel="stylesheet" href={`${config.FRONT_END_URL}css/bootstrap.min.css`}/>
+                    {/* <link rel="stylesheet" href={`${config.FRONT_END_URL}css/bootstrap.css`}/>
+                    <link rel="stylesheet" href={`${config.FRONT_END_URL}css/bootstrap.min.css`}/> */}
                     <link 
                         rel="stylesheet" 
                         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
